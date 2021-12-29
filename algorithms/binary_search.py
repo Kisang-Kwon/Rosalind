0a30bb44109d5b4f28458428529b938db123b2f1
권기상 <kwonkisang@gwongisang-ui-MacBookAir.local>
2021-12-29 09:19:34 +0900

    20211229

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,5 +1,6 @@
+import os
 import sys
-import math
+
 from typing import Tuple
 
 
@@ -9,28 +10,46 @@ def parse_input(filepath: str) -> Tuple:
         m = int(filehandle.readline())
         sorted_array = list(map(int, filehandle.readline().split()))
         integer_list = list(map(int, filehandle.readline().split()))
-        
+
     return n, m, sorted_array, integer_list
 
 
 def binary_search(k, start_idx, end_idx, sorted_array):
-    median_idx = math.ceil((end_idx-start_idxb)/2)
-    if k == sorted_array[median_idx-1]:
+    median_idx = (start_idx + end_idx) // 2
+
+    if k == sorted_array[median_idx - 1]:
         return median_idx
     else:
         if end_idx - start_idx == 1:
-            return -1
-        elif k > sorted_array[median_idx-1]:
-            start_idx = median_idx + 1
+            if k == sorted_array[start_idx - 1]:
+                return start_idx
+            elif k == sorted_array[end_idx - 1]:
+                return end_idx
+            else:
+                return -1
+        elif k > sorted_array[median_idx - 1]:
+            if end_idx > median_idx + 1:
+                start_idx = median_idx + 1
+            else:
+                start_idx = median_idx
             return binary_search(k, start_idx, end_idx, sorted_array)
         else:
-            end_idx = median_idx - 1
+            if start_idx < median_idx - 1:
+                end_idx = median_idx - 1
+            else:
+                end_idx = median_idx
             return binary_search(k, start_idx, end_idx, sorted_array)
 
 
 if __name__ == "__main__":
+    sys.setrecursionlimit(25000)
+
     filepath = sys.argv[1]
     n, m, sorted_array, integer_list = parse_input(filepath)
-    for k in integer_list:
-        idx = binary_search(k, 1, n, sorted_array)
-        print(idx+1)
+    base_path = os.path.dirname(os.path.dirname(__file__))
+    output_path = os.path.join(base_path, "outputs", "binary_search.txt")
+
+    with open(output_path, "w") as output:
+        for k in integer_list:
+            idx = binary_search(k, 1, n, sorted_array)
+            output.write(f"{idx} ")
